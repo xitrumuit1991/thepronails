866d620615b8d6a6f36e2ed3240155c13d74610a
unknown <txvinh.it@gmail.com>
2016-05-18 23:22:41 +0700

    n_v_style.css

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://localhost/thepronails/admin/');
 define('HTTPS_CATALOG', 'http://localhost/thepronails/');
 
 // DIR
-define('DIR_APPLICATION', 'admin/');
-define('DIR_SYSTEM', 'system/');
-define('DIR_IMAGE', 'image/');
-define('DIR_LANGUAGE', 'admin/language/');
-define('DIR_TEMPLATE', 'admin/view/template/');
-define('DIR_CONFIG', 'system/config/');
-define('DIR_CACHE', 'system/storage/cache/');
-define('DIR_DOWNLOAD', 'system/storage/download/');
-define('DIR_LOGS', 'system/storage/logs/');
-define('DIR_MODIFICATION', 'system/storage/modification/');
-define('DIR_UPLOAD', 'system/storage/upload/');
-define('DIR_CATALOG', 'catalog/');
+define('DIR_APPLICATION', '../admin/');
+define('DIR_SYSTEM', '../system/');
+define('DIR_IMAGE', '../image/');
+define('DIR_LANGUAGE', '../admin/language/');
+define('DIR_TEMPLATE', '../admin/view/template/');
+define('DIR_CONFIG', '../system/config/');
+define('DIR_CACHE', '../system/storage/cache/');
+define('DIR_DOWNLOAD', '../system/storage/download/');
+define('DIR_LOGS', '../system/storage/logs/');
+define('DIR_MODIFICATION', '../system/storage/modification/');
+define('DIR_UPLOAD', '../system/storage/upload/');
+define('DIR_CATALOG', '../catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
