2480cf6ae630eb8220f29a9184ef75b9b670d307
nguyen.tran <nguyen.tran@softfoundry.com>
2016-05-23 00:30:21 +0700

    A

---
+++ config.php
@@ -0,0 +1,96 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://pronailsupplyaz.com/');
+define('HTTPS_SERVER', 'http://pronailsupplyaz.com/');
+
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '107.180.27.158');
+define('DB_USERNAME', 'thepronails');
+define('DB_PASSWORD', 'thepronails@123');
+define('DB_DATABASE', 'thepronails');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+
+define('DIR_APPLICATION', 		'catalog/');
+define('DIR_SYSTEM', 			'system/');
+define('DIR_IMAGE',  			'image/');
+define('DIR_LANGUAGE', 			'catalog/language/');
+define('DIR_TEMPLATE', 			'catalog/view/theme/');
+define('DIR_CONFIG', 			'system/config/');
+define('DIR_CACHE', 				'system/storage/cache/');
+define('DIR_DOWNLOAD', 			'system/storage/download/');
+define('DIR_LOGS', 				'system/storage/logs/');
+define('DIR_MODIFICATION', 		'system/storage/modification/');
+define('DIR_UPLOAD', 			'system/storage/upload/');
+
+
+
+
+
+
+// define('HTTP_SERVER', 'http://localhost/thepronails/');
+// define('HTTPS_SERVER', 'http://localhost/thepronails/');
+
+// define('DIR_APPLICATION', 		'catalog/');
+// define('DIR_SYSTEM', 			'system/');
+// define('DIR_IMAGE',  			'image/');
+// define('DIR_LANGUAGE', 			'catalog/language/');
+// define('DIR_TEMPLATE', 			'catalog/view/theme/');
+// define('DIR_CONFIG', 			'system/config/');
+// define('DIR_CACHE', 				'system/storage/cache/');
+// define('DIR_DOWNLOAD', 			'system/storage/download/');
+// define('DIR_LOGS', 				'system/storage/logs/');
+// define('DIR_MODIFICATION', 	'system/storage/modification/');
+// define('DIR_UPLOAD', 			'system/storage/upload/');
+
+// Local
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'thepronails');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
+
+
+// DB host badger.vodien.com xitrumuit1991@ */
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'badger.vodien.com');
+// define('DB_USERNAME', 'topnotch_nail');
+// define('DB_PASSWORD', 'xitrumuit1991@');
+// define('DB_DATABASE', 'topnotch_nails');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
+
+
+
+// DB host host5.inet.vn xitrumuit1991 */
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'host5.inet.vn');
+// define('DB_USERNAME', 'nhabanbi_pronail');
+// define('DB_PASSWORD', 'xitrumuit1991');
+// define('DB_DATABASE', 'nhabanbi_thepronails');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
+
+
+
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', '115.75.7.43');
+// define('DB_USERNAME', 'pronails');
+// define('DB_PASSWORD', 'thepronails@7978');
+// define('DB_DATABASE', 'thepronails');
+// define('DB_PORT', '17799');
+// define('DB_PREFIX', 'oc_');
+
+
+
+//  DB host congty Vinh 
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'thepronails');
+// define('DB_PASSWORD', 'thepronails@123');
+// define('DB_DATABASE', 'thepronails');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
