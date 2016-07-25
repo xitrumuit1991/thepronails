b121148d2c0c78d4d6eb005f1749ee1859d3a273
xitrumuit1991 <xitrumuit1991@github.com>
2016-07-25 13:24:02 +0700

    done

--- admin/config.php
+++ admin/config.php
@@ -21,20 +21,21 @@ define('HTTPS_SERVER', 'http://localhost/thepronails/admin/');
 define('HTTPS_CATALOG', 'http://localhost/thepronails/');
 
 
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'thepronails');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
-
-
-
 // define('DB_DRIVER', 'mysqli');
-// define('DB_HOSTNAME', '107.180.27.158'); // define('DB_HOSTNAME', 'localhost'); 
-// define('DB_USERNAME', 'thepronails');
-// define('DB_PASSWORD', 'thepronails@123');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
 // define('DB_DATABASE', 'thepronails');
 // define('DB_PORT', '3306');
 // define('DB_PREFIX', 'oc_');
+
+
+
+define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', '107.180.27.158'); 
+define('DB_HOSTNAME', 'localhost'); 
+define('DB_USERNAME', 'thepronails');
+define('DB_PASSWORD', 'thepronails@123');
+define('DB_DATABASE', 'changestyle');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
