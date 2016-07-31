09970ecd26e42b8ea81f92bba12f6f392e5f86fc
xitrumuit1991 <xitrumuit1991@github.com>
2016-07-31 14:45:27 +0700

    config db

--- admin/config.php
+++ admin/config.php
@@ -1,58 +1,54 @@
 <?php
 
-define('DIR_APPLICATION', '../admin/');
-define('DIR_SYSTEM', '../system/');
-define('DIR_IMAGE', '../image/');
-define('DIR_LANGUAGE', '../admin/language/');
-define('DIR_TEMPLATE', '../admin/view/template/');
-define('DIR_CONFIG', '../system/config/');
-define('DIR_CACHE', '../system/storage/cache/');
-define('DIR_DOWNLOAD', '../system/storage/download/');
-define('DIR_LOGS', '../system/storage/logs/');
-define('DIR_MODIFICATION', '../system/storage/modification/');
-define('DIR_UPLOAD', '../system/storage/upload/');
-define('DIR_CATALOG', '../catalog/');
+define('DIR_APPLICATION', 		'../admin/');
+define('DIR_SYSTEM', 			'../system/');
+define('DIR_IMAGE', 			'../image/');
+define('DIR_LANGUAGE', 			'../admin/language/');
+define('DIR_TEMPLATE', 			'../admin/view/template/');
+define('DIR_CONFIG', 			'../system/config/');
+define('DIR_CACHE', 			'../system/storage/cache/');
+define('DIR_DOWNLOAD', 			'../system/storage/download/');
+define('DIR_LOGS', 				'../system/storage/logs/');
+define('DIR_MODIFICATION', 		'../system/storage/modification/');
+define('DIR_UPLOAD', 			'../system/storage/upload/');
+define('DIR_CATALOG', 			'../catalog/');
 
 
-//local host admin config
-// define('HTTP_SERVER', 'http://localhost/thepronails/admin/');
-// define('HTTP_CATALOG', 'http://localhost/thepronails/');
+/* //local host admin config   */
+define('HTTP_SERVER', 'http://localhost/thepronails/admin/');
+define('HTTP_CATALOG', 'http://localhost/thepronails/');
 
-// define('HTTPS_SERVER', 'http://localhost/thepronails/admin/');
-// define('HTTPS_CATALOG', 'http://localhost/thepronails/');
-//------------------------------------------------------
+define('HTTPS_SERVER', 'http://localhost/thepronails/admin/');
+define('HTTPS_CATALOG', 'http://localhost/thepronails/');
 
 
-//server admin config
-define('HTTP_SERVER', 'http://tinanailsupply.com/changestyle/admin/');
-define('HTTP_CATALOG', 'http://tinanailsupply.com/changestyle/');
 
-define('HTTPS_SERVER', 'http://tinanailsupply.com/changestyle/admin/');
-define('HTTPS_CATALOG', 'http://tinanailsupply.com/changestyle/');
-//-----------------------------------------------------------
-
+/* // host tinanailsupply.com/index.php */
+// define('HTTP_SERVER', 'http://tinanailsupply.com/admin/');
+// define('HTTP_CATALOG', 'http://tinanailsupply.com/');
+// define('HTTPS_SERVER', 'http://tinanailsupply.com/admin/');
+// define('HTTPS_CATALOG', 'http://tinanailsupply.com/');
 
 
 
 
 
 
+/* //host Localhost */
+define('DB_DRIVER', 			'mysqli');
+define('DB_HOSTNAME', 		'localhost');
+define('DB_USERNAME', 		'root');
+define('DB_PASSWORD', 		'');
+define('DB_DATABASE', 		'thepronails');
+define('DB_PORT', 			'3306');
+define('DB_PREFIX', 			'oc_');
 
-// define('DB_DRIVER', 'mysqli');
-// define('DB_HOSTNAME', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_DATABASE', 'thepronails');
-// define('DB_PORT', '3306');
-// define('DB_PREFIX', 'oc_');
-
-
-
-define('DB_DRIVER', 'mysqli');
-// define('DB_HOSTNAME', '107.180.27.158'); 
-define('DB_HOSTNAME', 'localhost'); 
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'thepronails');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+/* // host tinanailsupply.com/index.php */
+// define('DB_DRIVER', 			'mysqli');
+// // define('DB_HOSTNAME', 		'107.180.27.158'); 
+// define('DB_HOSTNAME', 			'localhost'); 
+// define('DB_USERNAME', 			'thepronails');
+// define('DB_PASSWORD', 			'thepronails@123');
+// define('DB_DATABASE', 			'thepronails');
+// define('DB_PORT', 				'3306');
+// define('DB_PREFIX', 			'oc_');
--- config.php
+++ config.php
@@ -5,31 +5,40 @@ define('DIR_IMAGE',  			'image/');
 define('DIR_LANGUAGE', 			'catalog/language/');
 define('DIR_TEMPLATE', 			'catalog/view/theme/');
 define('DIR_CONFIG', 			'system/config/');
-define('DIR_CACHE', 				'system/storage/cache/');
+define('DIR_CACHE', 			'system/storage/cache/');
 define('DIR_DOWNLOAD', 			'system/storage/download/');
 define('DIR_LOGS', 				'system/storage/logs/');
-define('DIR_MODIFICATION', 	'system/storage/modification/');
+define('DIR_MODIFICATION', 		'system/storage/modification/');
 define('DIR_UPLOAD', 			'system/storage/upload/');
 
 
 define('HTTP_SERVER', 'http://localhost/thepronails/');
 define('HTTPS_SERVER', 'http://localhost/thepronails/');
 
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'thepronails');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+// define('HTTP_SERVER', 'http://tinanailsupply.com/');
+// define('HTTPS_SERVER', 'http://tinanailsupply.com/');
 
 
 
-// define('DB_DRIVER', 'mysqli');
-// // define('DB_HOSTNAME', '107.180.27.158'); 
-// define('DB_HOSTNAME', 'localhost'); 
-// define('DB_USERNAME', 'thepronails');
-// define('DB_PASSWORD', 'thepronails@123');
-// define('DB_DATABASE', 'changestyle');
-// define('DB_PORT', '3306');
-// define('DB_PREFIX', 'oc_');
+
+
+/* //host Localhost */
+define('DB_DRIVER', 			'mysqli');
+define('DB_HOSTNAME', 			'localhost');
+define('DB_USERNAME', 			'root');
+define('DB_PASSWORD', 			'');
+define('DB_DATABASE', 			'thepronails');
+define('DB_PORT', 				'3306');
+define('DB_PREFIX', 			'oc_');
+
+
+
+/* // host tinanailsupply.com/index.php */
+// define('DB_DRIVER', 			'mysqli');
+// // define('DB_HOSTNAME', 	'107.180.27.158'); 
+// define('DB_HOSTNAME', 		'localhost'); 
+// define('DB_USERNAME', 		'thepronails');
+// define('DB_PASSWORD', 		'thepronails@123');
+// define('DB_DATABASE', 		'thepronails');
+// define('DB_PORT', 			'3306');
+// define('DB_PREFIX', 			'oc_');
