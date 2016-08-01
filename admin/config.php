f6140310975ca9318775fadb70520a38b0fda6bb
xitrumuit1991 <xitrumuit1991@github.com>
2016-08-01 09:16:45 +0700

    done edit config

--- admin/config.php
+++ admin/config.php
@@ -15,19 +15,18 @@ define('DIR_CATALOG', 			'../catalog/');
 
 
 /* //local host admin config   */
-define('HTTP_SERVER', 'http://localhost/thepronails/admin/');
-define('HTTP_CATALOG', 'http://localhost/thepronails/');
-
-define('HTTPS_SERVER', 'http://localhost/thepronails/admin/');
-define('HTTPS_CATALOG', 'http://localhost/thepronails/');
+// define('HTTP_SERVER', 'http://localhost/thepronails/admin/');
+// define('HTTP_CATALOG', 'http://localhost/thepronails/');
+// define('HTTPS_SERVER', 'http://localhost/thepronails/admin/');
+// define('HTTPS_CATALOG', 'http://localhost/thepronails/');
 
 
 
 /* // host tinanailsupply.com/index.php */
-// define('HTTP_SERVER', 'http://tinanailsupply.com/admin/');
-// define('HTTP_CATALOG', 'http://tinanailsupply.com/');
-// define('HTTPS_SERVER', 'http://tinanailsupply.com/admin/');
-// define('HTTPS_CATALOG', 'http://tinanailsupply.com/');
+define('HTTP_SERVER', 'http://tinanailsupply.com/changestyle/admin/');
+define('HTTP_CATALOG', 'http://tinanailsupply.com/changestyle/');
+define('HTTPS_SERVER', 'http://tinanailsupply.com/changestyle/admin/');
+define('HTTPS_CATALOG', 'http://tinanailsupply.com/changestyle/');
 
 
 
@@ -35,20 +34,20 @@ define('HTTPS_CATALOG', 'http://localhost/thepronails/');
 
 
 /* //host Localhost */
-define('DB_DRIVER', 			'mysqli');
-define('DB_HOSTNAME', 		'localhost');
-define('DB_USERNAME', 		'root');
-define('DB_PASSWORD', 		'');
-define('DB_DATABASE', 		'thepronails');
-define('DB_PORT', 			'3306');
-define('DB_PREFIX', 			'oc_');
+// define('DB_DRIVER', 			'mysqli');
+// define('DB_HOSTNAME', 		'localhost');
+// define('DB_USERNAME', 		'root');
+// define('DB_PASSWORD', 		'');
+// define('DB_DATABASE', 		'thepronails');
+// define('DB_PORT', 			'3306');
+// define('DB_PREFIX', 			'oc_');
 
 /* // host tinanailsupply.com/index.php */
-// define('DB_DRIVER', 			'mysqli');
-// // define('DB_HOSTNAME', 		'107.180.27.158'); 
-// define('DB_HOSTNAME', 			'localhost'); 
-// define('DB_USERNAME', 			'thepronails');
-// define('DB_PASSWORD', 			'thepronails@123');
-// define('DB_DATABASE', 			'thepronails');
-// define('DB_PORT', 				'3306');
-// define('DB_PREFIX', 			'oc_');
+define('DB_DRIVER', 			'mysqli');
+// define('DB_HOSTNAME', 		'107.180.27.158'); 
+define('DB_HOSTNAME', 			'localhost'); 
+define('DB_USERNAME', 			'thepronails');
+define('DB_PASSWORD', 			'thepronails@123');
+define('DB_DATABASE', 			'thepronails');
+define('DB_PORT', 				'3306');
+define('DB_PREFIX', 			'oc_');
